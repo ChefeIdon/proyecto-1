8ef71f8a182ec4f741e69023eca69d92eb015391
Federico Agra <ChefeIdon@users.noreply.github.com>
2020-05-13 20:36:28 -0300

    Mr. Omegoo - Diseño y conversión Color-Valor funcionando
    - Se definió el diseño de la aplicación web, y se le aplicó a la parte de conversión de código de colores a valor.
    - Se agregaron las páginas correspondientes para la conversión de código de colores a valor de resistencias con 5 y 6 bandas (aún no funcionando).
    - Conversion de código de colores a valor finalizada.
    - Se agrego un nombre y un logo a la aplicación web.

--- js/app.js
+++ js/app.js
@@ -2,54 +2,54 @@
 var resistencia = new Array();
 var seleccion = "";
 var imagen = "";
+var tolerancia = "±10%";
 
 //Iniciamos la resistencia con la configuracion inicial
 resistencia[0] = 0;
 resistencia[1] = 0;
 resistencia[2] = 1;
-resistencia[3] = 10;
 
 function seleccionarColor1(elemento)
 {
     seleccion = elemento.title;    
     switch(seleccion) {
-        case "0":
+        case "Negro":
             imagen = "imagenes/resistencias/cabeza-negro.png";
             resistencia[0] = 0;
             break;
-        case "1":
+        case "Marrón":
             imagen = "imagenes/resistencias/cabeza-marron.png";
             resistencia[0] = 1;
             break;
-        case "2":
+        case "Rojo":
             imagen = "imagenes/resistencias/cabeza-rojo.png";
             resistencia[0] = 2;
             break;
-        case "3":
+        case "Naranja":
             imagen = "imagenes/resistencias/cabeza-naranja.png";
             resistencia[0] = 3;
             break;
-        case "4":
+        case "Amarillo":
             imagen = "imagenes/resistencias/cabeza-amarillo.png";
             resistencia[0] = 4;
             break;
-        case "5":
+        case "Verde":
             imagen = "imagenes/resistencias/cabeza-verde.png";
             resistencia[0] = 5;
             break;
-        case "6":
+        case "Azul":
             imagen = "imagenes/resistencias/cabeza-azul.png";
             resistencia[0] = 6;
             break;
-        case "7":
+        case "Violeta":
             imagen = "imagenes/resistencias/cabeza-violeta.png";
             resistencia[0] = 7;
             break;
-        case "8":
+        case "Gris":
             imagen = "imagenes/resistencias/cabeza-gris.png";
             resistencia[0] = 8;
             break;
-        case "9":
+        case "Blanco":
             imagen = "imagenes/resistencias/cabeza-blanco.png";
             resistencia[0] = 9;
             break;
@@ -61,43 +61,43 @@ function seleccionarColor2(elemento)
 {
     seleccion = elemento.title;    
     switch(seleccion) {
-        case "0":
+        case "Negro":
             imagen = "imagenes/resistencias/medio-negro.png";
             resistencia[1] = 0;
             break;
-        case "1":
+        case "Marrón":
             imagen = "imagenes/resistencias/medio-marron.png";
             resistencia[1] = 1;
             break;
-        case "2":
+        case "Rojo":
             imagen = "imagenes/resistencias/medio-rojo.png";
             resistencia[1] = 2;
             break;
-        case "3":
+        case "Naranja":
             imagen = "imagenes/resistencias/medio-naranja.png";
             resistencia[1] = 3;
             break;
-        case "4":
+        case "Amarillo":
             imagen = "imagenes/resistencias/medio-amarillo.png";
             resistencia[1] = 4;
             break;
-        case "5":
+        case "Verde":
             imagen = "imagenes/resistencias/medio-verde.png";
             resistencia[1] = 5;
             break;
-        case "6":
+        case "Azul":
             imagen = "imagenes/resistencias/medio-azul.png";
             resistencia[1] = 6;
             break;
-        case "7":
+        case "Violeta":
             imagen = "imagenes/resistencias/medio-violeta.png";
             resistencia[1] = 7;
             break;
-        case "8":
+        case "Gris":
             imagen = "imagenes/resistencias/medio-gris.png";
             resistencia[1] = 8;
             break;
-        case "9":
+        case "Blanco":
             imagen = "imagenes/resistencias/medio-blanco.png";
             resistencia[1] = 9;
             break;
@@ -109,51 +109,51 @@ function seleccionarMultiplicador(elemento)
 {
     seleccion = elemento.title;    
     switch(seleccion) {
-        case "x1Ω":
+        case "Negro":
             imagen = "imagenes/resistencias/medio-negro.png";
             resistencia[2] = 1;
             break;
-        case "x10Ω":
+        case "Marrón":
             imagen = "imagenes/resistencias/medio-marron.png";
             resistencia[2] = 10;
             break;
-        case "x100Ω":
+        case "Rojo":
             imagen = "imagenes/resistencias/medio-rojo.png";
             resistencia[2] = 100;
             break;
-        case "x1KΩ":
+        case "Naranja":
             imagen = "imagenes/resistencias/medio-naranja.png";
             resistencia[2] = 1000;
             break;
-        case "x10KΩ":
+        case "Amarillo":
             imagen = "imagenes/resistencias/medio-amarillo.png";
             resistencia[2] = 10000;
             break;
-        case "x100KΩ":
+        case "Verde":
             imagen = "imagenes/resistencias/medio-verde.png";
             resistencia[2] = 100000;
             break;
-        case "x1MΩ":
+        case "Azul":
             imagen = "imagenes/resistencias/medio-azul.png";
             resistencia[2] = 1000000;
             break;
-        case "x10MΩ":
+        case "Violeta":
             imagen = "imagenes/resistencias/medio-violeta.png";
             resistencia[2] = 10000000;
             break;
-        case "x100MΩ":
+        case "Gris":
             imagen = "imagenes/resistencias/medio-gris.png";
             resistencia[2] = 100000000;
             break;
-        case "x1GΩ":
+        case "Blanco":
             imagen = "imagenes/resistencias/medio-blanco.png";
             resistencia[2] = 1000000000;
             break;
-        case "x0.01Ω":
+        case "Plata":
             imagen = "imagenes/resistencias/medio-plata.png";
             resistencia[2] = 0.01;
             break;
-        case "x0.1Ω":
+        case "Oro":
             imagen = "imagenes/resistencias/medio-oro.png";
             resistencia[2] = 0.1;
             break;
@@ -165,29 +165,37 @@ function seleccionarTolerancia(elemento)
 {
     seleccion = elemento.title;
     switch(seleccion) {
-        case "±1%":
-            imagen = "imagenes/resistencias/cola-marron.png"
+        case "Marrón":
+            imagen = "imagenes/resistencias/cola-marron.png";
+            tolerancia = "±1%";
             break;
-        case "±2%":
-            imagen = "imagenes/resistencias/cola-rojo.png"
+        case "Rojo":
+            imagen = "imagenes/resistencias/cola-rojo.png";
+            tolerancia = "±2%";
             break;
-        case "±0.5%":
-            imagen = "imagenes/resistencias/cola-verde.png"
+        case "Verde":
+            imagen = "imagenes/resistencias/cola-verde.png";
+            tolerancia = "±0.5%";
             break;
-        case "±0.25%":
-            imagen = "imagenes/resistencias/cola-azul.png"
+        case "Azul":
+            imagen = "imagenes/resistencias/cola-azul.png";
+            tolerancia = "±0.25%";
             break;
-        case "±0.1%":
-            imagen = "imagenes/resistencias/cola-violeta.png"
+        case "Violeta":
+            imagen = "imagenes/resistencias/cola-violeta.png";
+            tolerancia = "±0.1%";
             break;
-        case "±0.05%":
-            imagen = "imagenes/resistencias/cola-gris.png"
+        case "Gris":
+            imagen = "imagenes/resistencias/cola-gris.png";
+            tolerancia = "±0.05%";
             break;
-        case "±5%":
-            imagen = "imagenes/resistencias/cola-plata.png"
+        case "Plata":
+            imagen = "imagenes/resistencias/cola-plata.png";
+            tolerancia = "±5%";
             break;
-        case "±10%":
+        case "Oro":
             imagen = "imagenes/resistencias/cola-oro.png"
+            tolerancia = "±10%";
             break;
     }
     document.getElementById("seleccion-color4").childNodes[1].src=imagen;
@@ -197,6 +205,7 @@ function seleccionarTolerancia(elemento)
 function calcular4bandas()
 {
     var resultado = (resistencia[0]*10 + resistencia[1]) * resistencia[2];
+    var unidad = "Ω";
     if(resultado > 1000)
     {
         resultado = resultado/1000;
@@ -206,29 +215,27 @@ function calcular4bandas()
             if(resultado > 1000)
             {
                 resultado = resultado/1000;
-                document.getElementById("nodo-calculo").childNodes[3].innerHTML = resultado + " GΩ";
+                unidad = " GΩ";
             }
             else
             {
-                document.getElementById("nodo-calculo").childNodes[3].innerHTML = resultado + " MΩ";
+                unidad = " MΩ";
             }
         }
         else
         {
-            document.getElementById("nodo-calculo").childNodes[3].innerHTML = resultado + " KΩ";
+            unidad = " KΩ";
         }
     }
     else
     {
         if(resultado < 10)
         {
-            document.getElementById("nodo-calculo").childNodes[3].innerHTML = resultado.toFixed(2) + " Ω";
-        }
-        else
-        {
-            document.getElementById("nodo-calculo").childNodes[3].innerHTML = resultado + " Ω";
+            resultado.toFixed(2);
         }
 
     }
+    resultado = resultado + unidad + " Tolerancia:" + tolerancia;
+    document.getElementById("nodo-calculo").childNodes[3].innerHTML = resultado;
 
 }
